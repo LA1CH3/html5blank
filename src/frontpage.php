b7e5f9fd11dacaf8cfefe853a730bd1ffef78e5c
Jay Laiche <jay@jaylaiche.com>
2015-05-23 19:36:11 -0400

    removed unnecessary files

---
+++ src/frontpage.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+	<main role="main">
+		<!-- section -->
+		<section>
+
+
+		</section>
+		<!-- /section -->
+	</main>
+
+<?php get_footer(); ?>
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,7 +2,20 @@
 	"use strict";
 
 	$(function () {
-		// DOM ready, take it away
+
+		// cache selectors here as properties of cache object
+		// example: $cache.headingOnes = $("h1");
+		var $cache = {};
+
+		// Init any jquery plugins here
+		// example: $(".slider").slick({....});
+
+
+		// Any custom functions can go here
+		// example myFunc(a,b){ ... }
+
+		// All other needs can go here
+		
 	});
 
 } ( this, jQuery ));
